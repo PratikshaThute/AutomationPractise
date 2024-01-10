3e9c4fef93b785fb903c39e75a0eb79f7cb129b0
PratikshaThute <155456788+PratikshaThute@users.noreply.github.com>
2024-01-10 14:50:39 +0530

    Added new class LinkedIn and folder Target
    Added the class LinkedIn and saved the screenshot in the folder created name as Target.

--- SeleniumMaven/src/main/java/SeleniumMavenProject/SeleniumMaven/base_class.java
+++ SeleniumMaven/src/main/java/SeleniumMavenProject/SeleniumMaven/base_class.java
@@ -1,31 +1,49 @@
 package SeleniumMavenProject.SeleniumMaven;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.io.FileHandler;
 
 public class base_class {
 	public static WebDriver driver;
+	public static String Projectpath = System.getProperty("user.dir");
 
 	public static void launch_browser(String browser) {
 
 		if (browser.equalsIgnoreCase("Chrome")) {
 			driver = new ChromeDriver();
-			System.out.println("Launched browser is :- "+ browser);
-			
+			System.out.println("Launched browser is :- " + browser);
+
 		} else if (browser.equalsIgnoreCase("firefox")) {
 			driver = new FirefoxDriver();
-			System.out.println("Launched browser is :- "+ browser);
+			System.out.println("Launched browser is :- " + browser);
 
 		} else if (browser.equalsIgnoreCase("edge")) {
 			driver = new EdgeDriver();
-			System.out.println("Launched browser is :- "+ browser);
+			System.out.println("Launched browser is :- " + browser);
 
 		}
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	}
+
+	public static void navigate(String url) {
+		driver.get(url);
+		System.out.println("Navigate Url is :-" + url);
+	}
+
+	public static void takescreenshot(String screenshotname) throws Exception {
+		TakesScreenshot tc = (TakesScreenshot) driver;
+		File Source = tc.getScreenshotAs(OutputType.FILE);
+		File Target = new File(Projectpath + "\\target\\" + screenshotname + ".png");
+		FileHandler.copy(Source, Target);
 	}
 }
---
+++ SeleniumMaven/src/test/java/linkedIn_app/package-info.java
@@ -0,0 +1,1 @@
+package linkedIn_app;
