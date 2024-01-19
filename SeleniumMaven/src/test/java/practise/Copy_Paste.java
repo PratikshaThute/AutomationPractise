bd195fc1de8e5c6e9308f72e906fcf605a689dae
PratikshaThute <155456788+PratikshaThute@users.noreply.github.com>
2024-01-19 16:38:48 +0530

    Added class CopyPaste in practise package
    Added the class CopyPaste and folder as CopyPaste_Evidence in order to add screenshot for the same.

---
+++ SeleniumMaven/src/test/java/practise/Copy_Paste.java
@@ -0,0 +1,41 @@
+package practise;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+import SeleniumMavenProject.SeleniumMaven.base_class;
+
+public class Copy_Paste extends base_class {
+
+	public static void main(String[] args) throws Exception {
+		launch_browser("Chrome");
+		navigate("https://www.naukri.com/");
+		takescreenshot("Naukri - homepage");
+
+		WebElement Login = driver.findElement(By.id("login_Layer"));
+
+		Actions act = new Actions(driver);
+
+		act.moveToElement(Login).click().build().perform();
+		takescreenshot("Naukri - click on login");
+		
+		WebElement email = driver.findElement(By.xpath("//input[@placeholder='Enter your active Email ID / Username']"));
+		email.sendKeys("pthute29@gmail.com");
+		takescreenshot("naukri - enter username");
+
+		act.moveToElement(email).keyDown(Keys.CONTROL).sendKeys("a").keyUp(Keys.CONTROL).build().perform();
+		act.keyDown(Keys.CONTROL).sendKeys("c").keyUp(Keys.CONTROL).build().perform();
+
+		takescreenshot("Naukri - select the username");
+		act.sendKeys(Keys.TAB).build().perform();
+	
+		takescreenshot("Naukri - password");
+		act.keyDown(Keys.CONTROL).sendKeys("v").keyUp(Keys.CONTROL).build().perform();
+
+		takescreenshot("Naukri - enter password");
+
+	}
+
+}
