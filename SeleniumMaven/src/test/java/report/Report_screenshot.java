590398385209a06125578d0703052ee8abf370f0
PratikshaThute <155456788+PratikshaThute@users.noreply.github.com>
2024-02-02 18:58:12 +0530

    Added class Report_Screenshot
    Added class Report_Screenshot and folder ReportEvidences and report Testing to the Reports Folder.

---
+++ SeleniumMaven/src/test/java/report/Report_screenshot.java
@@ -0,0 +1,54 @@
+package report;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
+import SeleniumMavenProject.SeleniumMaven.base_class;
+
+public class Report_screenshot extends base_class {
+	public static ExtentTest test;
+
+	public static void main(String[] args) throws Exception {
+
+		String Projectpath = System.getProperty("user.dir");
+
+		ExtentSparkReporter report = new ExtentSparkReporter(Projectpath + "\\Reports\\testing");
+		report.config().setDocumentTitle("Automation testing");
+		report.config().setReportName("Sanity Testing");
+		report.config().setTheme(Theme.DARK);
+
+		ExtentReports r = new ExtentReports();
+		r.attachReporter(report);
+		r.setSystemInfo("OS", "Windows");
+		r.setSystemInfo("Browser", "Chrome");
+		r.setSystemInfo("Test Engineer", "Pratiksha Thute");
+		r.setSystemInfo("Application", "Naukri.com");
+
+		launch_browser("chrome");
+		navigate("https://www.naukri.com/");
+		takescreenshot("Naukri-Homepage");
+
+		test = r.createTest("Naukri homepage");
+		test.log(Status.INFO, "Homepage verification");
+		test.log(Status.PASS, "Verification successful");
+		test.addScreenCaptureFromPath(Projectpath + "\\ReportEvidences\\Naukri-Homepage.png");
+
+		driver.findElement(By.xpath("//div[text()='Search']")).click();
+		takescreenshot("Naukri-Search");
+
+		test = r.createTest("Naukri Search button");
+		test.log(Status.INFO, "Search functionality");
+		test.log(Status.PASS, "Search clicked successfully");
+		test.addScreenCaptureFromPath(Projectpath + "\\ReportEvidences\\Naukri-Search.png");
+
+		r.flush();
+
+	}
+
+}
